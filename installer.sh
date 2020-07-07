6bec885bbf3cea2901310187d90a0dd465ae5920
abdul125 <abdulrahman.asiri.a@gmail.com>
2020-07-07 13:00:22 +0000

    removing 2 lines

--- installer.sh
+++ installer.sh
@@ -7,5 +7,3 @@ sudo apt-get jq
 sudo curl -L get.docker.com | sh
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x usr/local/bin/docker-compose
-sudo groupadd docker
-sudo usermod -aG docker $USER
