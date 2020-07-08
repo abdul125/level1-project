3a7e14d6b5976560f83b3a16854122025c1c3e9d
Ra-tnt <roody.tnt@gmail.com>
2020-07-08 13:46:15 +0000

    final

--- installer.sh
+++ installer.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
-sudo apt update 
-sudo apt install curl vim jq make
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
-sudo curl -L get.docker.com | sh
-sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo apt-get update
+sudo apt-get install docker.io curl vim jq make &&
+#sudo apt install curl vim jq make
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+#sudo curl -L get.docker.com | sh
+#sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x ../../../usr/local/bin/docker-compose
 sudo groupadd docker
 sudo usermod -aG docker $USER
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
