dcb29d9636a92b258bf9ba7a4c77ea729f2864d0
abdulrahman asiri <abdulrahman.asiri.a@gmail.com>
2020-07-12 15:18:48 +0000

    fix bugs in Makefiles target curl & housekeeping

--- installer.sh
+++ installer.sh
@@ -1,11 +1,8 @@
-#! /bin/bash
-sudo apt-get update
-sudo apt-get install docker.io curl vim jq make &&
-#sudo apt install curl vim jq make
+#!/bin/bash
+sudo apt update &&
+sudo apt install docker.io curl vim jq make -y &&
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-#sudo curl -L get.docker.com | sh
-#sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x ../../../usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 sudo groupadd docker
 sudo usermod -aG docker $USER
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
